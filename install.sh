96c7dac48ad5f282df2b7c2ed02b89fdab268cc9
volumio <volumio@localhost>
2018-11-15 22:15:30 +0000

    complete install

---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+cp addplaylist.js /use/local/bin/
+cp rotary_volume.py /usr/local/bin/
+cp rotary_volume.sh /etc/init.d/
+update-rc.d rotary_volume.sh defaults
+cp rotary_stations.py /usr/local/bin
+cp rotary_stations.sh /etc/init.d/
+update-rc.d rotary_stations.sh defaults
+cp led.py /usr/local/bin
+cp led.sh /etc/init.d/
+update-rc.d led.sh defaults
+cp startup-serv.sh /etc/init.d/
+update-rc.d startup-serv.sh defaults
+apt-get update
+apt-get install pip 
+apt-get install -y cron 
+pip install simplejson 
+pip install spotipy
+pip install pico2wav
+(crontab -l ; echo "0 3 * * * /home/volumio/build_playlists.py") | sort - | uniq - | crontab -
+
---
+++ led.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+import subprocess
+import json
+LedPin = 17
+LedPin2 = 5
+
+def setup():
+	GPIO.setmode(GPIO.BCM)      
+	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
+	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+	GPIO.setup(LedPin2, GPIO.OUT)   # Set LedPin's mode is output
+	GPIO.output(LedPin2, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+
+def loop():
+	while True:
+		try:
+			proc = subprocess.Popen(["volumio status"], stdout=subprocess.PIPE, shell=True)
+			(out, err) = proc.communicate()
+			status=json.loads(out)
+			if(status[u'status']=="play"):
+				GPIO.output(LedPin, GPIO.HIGH)  # led on
+				GPIO.output(LedPin2, GPIO.LOW)  # led on
+				time.sleep(1)
+				print 'led on...'
+			else :
+				print 'led off'
+				GPIO.output(LedPin, GPIO.LOW) # led off
+				GPIO.output(LedPin2, GPIO.HIGH) # led off
+				time.sleep(1)
+		except:
+			time.sleep(1)
+
+def destroy():
+	GPIO.output(LedPin, GPIO.HIGH)     # led off
+	GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
